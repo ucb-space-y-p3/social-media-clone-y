7e07cb2489e4ee50bd76a8892ad37d004dfadc2e
Koby <kobysac@gmail.com>
2024-01-18 23:14:42 -0800

    apollo server and sandbox working

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Chat = require('./Chat');
+const Message = require('./Message');
+
+module.exports = { User, Post, Comment, Chat, Message };
---
+++ server/utils/auth.js
@@ -0,0 +1,40 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh'; // hide!!!!!!!!!!!
+const expiration = '2h'; // hide!!!!!!!!!!!
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  // function for our authenticated routes
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
