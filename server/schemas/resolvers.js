fd74689bf38d970852aef74b2b91ce7ff643c8ad
Koby <kobysac@gmail.com>
2024-01-20 00:49:45 -0800

    added to typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,7 +12,7 @@ const resolvers = {
     },
   },
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
+    createUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,27 +4,57 @@ const typeDefs = `
     _id: ID
     username: String
     email: String
-    posts
-    friends
-    chats
-    friendCount
-    postCount
+    posts: [Post]
+    friends: [User]
+    chats: [Chat]
+    notifications: [Notification]
+    friendCount: Int
+    postCount: Int
+    settings: Settings
+  }
+
+  type Settings {
+    silentMode: Boolean
+  }
+
+  type Notification {
+    _id: ID
+    type: String
+    message: String
+    createdAt: String
+    isCleared: Boolean
   }
 
   type Post {
     _id: ID
-    creator:
-    likeCount:
-    commentCount:
-    cooments: [Comments]!
+    creator: String
+    content: String
+    createdAt: String
+    likeCount: Int
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID
+    postId: ID
+    creatorName: String
+    createdAt: String
+    content: String
   }
 
   type Chat {
-
+    _id: ID
+    isGroupChat: Boolean
+    currentRecipients: [User]
+    pastRecipients: [User]
+    messages: [Message]
   }
 
-  input xxxxInput {
-
+  type Message {
+    _id: ID
+    creator: String!
+    createdAt: String!
   }
 
   type Auth {
@@ -35,12 +65,30 @@ const typeDefs = `
   # Define which queries the front end is allowed to make and what data is returned
   type Query {
     me: User
+    getFriends: User
+    getPosts: [Post]
+    getComments: [Comment]
+    getChat(chatId: ID!): Chat
+    getChats: [Chat]
+    getNotifications: [Notification]
+    singleUser(username: String!): User
+    
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    editUser(username: String, email: String): User
+    changePassword(oldPassword: String!): User
+    deleteUser(username: String!, email: String!, password: String!): User
     login(email: String!, password: String!): Auth
-    addFriend(user: String!, newFriend: String!): User
+    addFriend(friend: String!): User
+    removeFriend(friend: String!): User
+    createPost(content: String!): Post
+    deletePost(postId: ID!): Post
+    createComment(postId: ID!, content: String!): Comment
+    createChat(recipientIds: [ID]!, firstMessage: String!): Chat
+    leaveChat(chatId: ID!): Chat
+    clearNotifications(notificationIds: [ID]!): [Notification]
   }
 `;
 
