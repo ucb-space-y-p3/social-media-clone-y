f93de01dc91e1334f47ecc43211d7f630ee274ee
Camsca <camila_vass@hotmail.com>
2024-01-23 21:40:28 -0800

    enabled suscriptions & installed correspondent dependencies

--- server/server.js
+++ server/server.js
@@ -3,21 +3,49 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { createServer } = require('http');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { WebSocketServer }=require( 'ws');
+const { useServer }=require( 'graphql-ws/lib/use/ws');
 
 const PORT = process.env.PORT || 3005;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+//create a new http server to work with subscriptions
+const httpServer = createServer(app);
+//create an executable Graphql schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+// new ws server
+const wsServer=new WebSocketServer({
+  server:httpServer,
+  path:'/suscriptions'//probchane
 });
-
+//use the ws en graphql-ws
+const serverCleanup = useServer(
+  {
+    schema,},wsServer);
 // Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
+const server = new ApolloServer({
+  schema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
+});
+async function startApolloServer() {
   await server.start();
 
+  // Authentication middleware
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
@@ -28,6 +56,7 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
+  // Production mode
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -37,12 +66,11 @@ const startApolloServer = async () => {
   }
 
   db.once('open', () => {
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
-};
+}
 
-// Call the async function to start the server
 startApolloServer();
