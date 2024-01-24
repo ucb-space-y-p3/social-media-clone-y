9f083a0afb9bc4dc7eaafdf6da90dc9cac115bbd
Koby <kobysac@gmail.com>
2024-01-23 21:02:03 -0800

    added a user slice and configured app.jsx to have apollo/rtk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        error: <ErrorNoResource />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }, {
+                path: '/login',
+                element: <Login />
+            }, {
+                path: '/signup',
+                element: <Signup />
+            }, {
+                path: '/chats',
+                element: <Chats />
+            }, {
+                path: '/notifications',
+                element: <Notifications />
+            }, {
+                path: '/user/:username',
+                element: <UserProfile />
+            }, {
+                path: '/me',
+                element: <UserProfile isMe/> // change maybe?
+            }, {
+                path: '/me/settings',
+                element: <Settings /> // change maybe?
+            }, 
+        ]
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <RouterProvider router={router} />
+);
---
+++ client/src/utils/queries.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+
+`;
+
+export const GET_USER = gql`
+
+`;
+
+export const GET_FRIENDS = gql`
+
+`;
+
+export const GET_FRIEND_REQUEST = gql`
+
+`;
+
+export const GET_FRIEND_REQUESTS = gql`
+
+`;
+
+export const GET_POST = gql`
+
+`;
+
+export const GET_POSTS = gql`
+
+`;
+
+export const GET_COMMENT = gql`
+
+`;
+
+export const GET_COMMENTS = gql`
+
+`;
+
+export const GET_CHAT = gql`
+
+`;
+
+export const GET_CHATS = gql`
+
+`;
+
+export const GET_MESSAGES = gql`
+
+`;
+
+export const GET_NOTIFICATIONS = gql`
+
+`;
