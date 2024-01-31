6126256f8e49c147c73d487717cb14b82dc1946d
Koby <kobysac@gmail.com>
2024-01-30 20:41:25 -0800

    fixing main

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -127,76 +127,14 @@ const resolvers = {
             throw PostNotFoundError;
           };
 
-    //   } catch (error) {
-    //     console.log(error);
-    //     throw error;
-    //   }
-    // },
-    // // not necessary
-    // getChat: async (parent, { }, context) => {
-    //   try {
-    //     // if (context.user) {
-
-    //     // }
-    //     // throw AuthenticationError;
-
-    //     // dev code
-
-    //   } catch (error) {
-    //     console.log(error);
-    //     throw error;
-    //   }
-    // },
-    // // not necessary
-    // getChats: async (parent, { }, context) => {
-    //   try {
-    //     // if (context.user) {
-
-    //     // }
-    //     // throw AuthenticationError;
-
-    //     // dev code
-
-    //   } catch (error) {
-    //     console.log(error);
-    //     throw error;
-    //   }
-    // },
-    // // not necessary
-    // getMessages: async (parent, { chatId }, context) => {
-    //   try {
-    //     // if (context.user) {
-
-    //     // }
-    //     // throw AuthenticationError;
-
-    //     // dev code
-
-    //   } catch (error) {
-    //     console.log(error);
-    //     throw error;
-    //   }
-    // },
-    // // not necessary
-    // getNotifications: async (parent, { }, context) => {
-    //   try {
-    //     // if (context.user) {
-
-    //     // }
-    //     // throw AuthenticationError;
-
-    //     // dev code
-
-    //   } catch (error) {
-    //     console.log(error);
-    //     throw error;
-    //   }
-    // },
-    // agith
-
-
-    // dev methods
-
+          return posts;
+        }
+        throw AuthenticationError;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
     // agith
 
     // not necessary
